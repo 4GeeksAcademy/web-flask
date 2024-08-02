05a47874c121621cf9ae5cc2c35e8febca9707f6
ValeriaVigo <vvigomiranda@gmail.com>
2024-08-02 14:55:47 -0500

    ahorasi

--- src/app.py
+++ src/app.py
@@ -1,22 +1,27 @@
 from flask import Flask, request, render_template
+import pickle
 import pandas as pd
-from waitress import serve
 
 app = Flask(__name__)
+
+# Cargar el modelo
+with open('models/model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/data', methods=['POST'])
-def data():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            df = pd.read_csv(file)
-            data_html = df.to_html()
-            return render_template('index.html', data=data_html)
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['file']
+    if file:
+        df = pd.read_csv(file)
+        predictions = model.predict(df)
+        df['Predictions'] = predictions
+        data_html = df.to_html()
+        return render_template('index.html', data=data_html)
     return render_template('index.html')
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
