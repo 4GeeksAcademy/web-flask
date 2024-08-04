d1a436029a27c4a4a0cf169270768bba38e8df3f
ValeriaVigo <vvigomiranda@gmail.com>
2024-08-04 14:38:10 -0500

    ok

--- src/app.py
+++ src/app.py
@@ -1,28 +1,34 @@
-from flask import Flask, request, render_template
-import pickle
-import pandas as pd
+from flask import Flask,request,render_template,url_for
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib as joblib
+import os
 
-app = Flask(__name__)
+model=joblib.load('model.pkl')
+scaler=joblib.load('model.sav')
 
-# Cargar el modelo
-with open('models/model.pkl', 'rb') as f:
-    model = pickle.load(f)
+app =Flask(__name__)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    file = request.files['file']
-    if file:
-        df = pd.read_csv(file)
-        predictions = model.predict(df)
-        df['Predictions'] = predictions
-        data_html = df.to_html()
-        return render_template('index.html', data=data_html)
-    return render_template('index.html')
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method =='POST':
+        sl=request.form['cons.price.idx']
+        sw = request.form['euribor3m']
+        pl = request.form['pdays']
+        pw = request.form['age']
+        data = np.array([[sl, sw, pl, pw]])
+        x = scaler.transform(data)
+        print(x)
+        prediction = model.predict(x)
+        print(prediction)
+    return render_template('index.html',prediction=prediction[0])
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
-
+    app.run(debug=True)
